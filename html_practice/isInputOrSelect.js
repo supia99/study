af42b30915597b7c16e33d42bc11b24a81fc1662
supia99 <3kzk9.vl.embox@gmail.com>
2019-01-25 07:05:58 +0900

    ii

---
+++ html_practice/isInputOrSelect.js
@@ -0,0 +1,10 @@
+
+  <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+/**
+ *
+*/
+function isInputOrSelect(element) {
+    if(element.is('input')) {
+      console.log("Yes, input.");
+    }
+}
