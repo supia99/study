5734283b5dd2967155ba868e02e6a711dcf1f24a
supia99 <3kzk9.vl.embox@gmail.com>
2021-07-20 18:04:55 +0900

    add: Radiko録音スクリプト作成。

--- bash/rec_AAndGPlus/recAAndGPlus.sh
+++ bash/rec_AAndGPlus/recAAndGPlus.sh
@@ -4,36 +4,15 @@
 # 引数
 #  1. 番組名
 #  2. 録音時間(秒)
-#  3. アップロード先ディレクトリ
 #---
 
 url=https://www.uniqueradio.jp/agplayer5/hls/mbr-0-cdn.m3u8
 
-# 録音データを読み込むNASのマウントディレクトリ
-SOURCE_NAS=/mnt/radikoTufNas
-# 録音データを読み込むディレクトリ
-UPLOAD_SOURCE=${SOURCE_NAS}/Temp/
-# 録音データをコピーする先のNASのマウントディレクトリ
-DEST_NAS=/mnt/supirDirLandiskNas
-# 録音データをコピーする先のディレクトリ
-UPLOAD_DEST=${DEST_NAS}/radiko/
+# TUF NAS のアップロード先
+UPLOAD_TUF=/mnt/radikoTufNas/Temp/
+
 # 録音データでラジオ局がわからなかった場合のコピー先ディレクトリ
-UPLOAD_OTHERS_AAndG=${UPLOAD_DEST}超A\&G/
-
-
-## マウント用ディレクトリ作成.ある場合は、作成しない
-if [ ! -d ${SOURCE_NAS} ]; then
-  mkdir ${SOURCE_NAS}
-fi
-if [ ! -d ${DEST_NAS} ]; then
-  mkdir ${DEST_NAS}
-fi
-
-
-## マウント
-sudo mount -t drvfs '\\192.168.50.1\radiko' ${SOURCE_NAS}
-sudo mount -t drvfs '\\landisk-c30161\supiaDir' ${DEST_NAS}
-
+UPLOAD_LANDISK=/mnt/supirDirLandiskNas/radiko/超A\&G/
 
 
 programName=$1
@@ -44,5 +23,5 @@ mkdir -p ~/tmp/radio
 fileName=${programName}_`date "+%Y%m%d-%H%M%S".mp3`
 ffmpeg -i ${url} -movflags faststart -t $((recTime+60)) ~/tmp/radio/${fileName}
 
-cp ~/tmp/radio/${fileName} ${UPLOAD_SOURCE}
-cp ~/tmp/radio/${fileName} ${UPLOAD_OTHERS_AAndG}
+cp ~/tmp/radio/${fileName} ${UPLOAD_TUF}
+cp ~/tmp/radio/${fileName} ${UPLOAD_LANDISK}
---
+++ bash/rec_AAndGPlus/recRadiko.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#---
+# 引数
+#　1. ラジオ局
+#  2. 番組名
+#  3. 録音時間(分)
+#---
+
+# TUF NAS のアップロード先
+UPLOAD_TUF=/mnt/radikoTufNas/Temp/
+
+# 録音データでラジオ局がわからなかった場合のコピー先ディレクトリ
+UPLOAD_LANDISK=/mnt/supirDirLandiskNas/radiko/
+
+radioStation=$1
+programName=$2
+recTime=$3
+
+mkdir -p ~/tmp/radio
+
+fileName=${programName}_`date "+%Y%m%d-%H%M%S"`
+/home/kawajiri/github/study/bash/rec_AAndGPlus/radish/radi.sh -t radiko -s ${radioStation} -d $((recTime+1)) ~/tmp/radio/${fileName}
+
+cp ~/tmp/radio/${fileName} ${UPLOAD_TUF}
+cp ~/tmp/radio/${fileName} ${UPLOAD_LANDISK}${radioStation}/
